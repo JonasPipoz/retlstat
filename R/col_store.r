3c57394c16dcb2ab4a099eab1c2c4a3ddd2edc62
JonasPipoz <48938512+JonasPipoz@users.noreply.github.com>
2020-06-11 16:03:06 +0200

    adding col_store function
    function to create column stored indexes

---
+++ R/col_store.r
@@ -0,0 +1,26 @@
+#
+#' col_store
+#'
+#' This function create a columns stored index on a specified table
+#' @param conn A DBI connection
+#' @param bdd The sql database name
+#' @param schema The sql schema name
+#' @param table The sql Table name
+#' @param drop default is FALSE, if True, drop the existing index first to create the new one.
+#' @export
+#' @examples col_store(conn = conn, BDD = 'STATPRODTEMP',Schema = 'GEN',table_name = 'MTCARS',drop = FALSE)
+#' col_store()
+#'
+
+col_store <- function(conn, bdd, schema, table_name, drop = FALSE ){
+  nom.indexe <- paste0("I_",table)
+  if(drop == FALSE){
+    dr <- 'OFF'
+  }else{
+    dr <- 'ON'
+  }
+  query <- paste0("CREATE CLUSTERED COLUMNSTORE INDEX [",nom.indexe ,"] ON [",schema,"].[",table,"] WITH  (DROP_EXISTING = ",dr,") ;")
+  #--default is OFF, Si il existe d?j? un index sur la table, il est possible de le remplacer par celui-ci ( DROP_EXISTING = ON)
+
+  odbc::dbSendQuery(conn,query)
+}
